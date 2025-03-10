d6b5b9ef5c424288fd7c1ebf1bc0ed2e3e0fc52c
Kevin-M254 <kmburu007@gmail.com>
2025-03-10 20:44:07 +0300

    ip to address

--- 0x04-networking_ruby/9-geoip.rb
+++ 0x04-networking_ruby/9-geoip.rb
@@ -2,12 +2,13 @@
 
 require 'geoip'
 ip = ARGV[0]
-geoip = GeoIP.new('GeoLiteCity.dat')
-geoinfo = geoip.country(ip).to_hash
-puts "IP address:\t"   + geoinfo[:ip]
-puts "Country:\t"      + geoinfo[:country_name]
-puts "Country code:\t" + geoinfo[:country_code2]
-puts "City name:\t"    + geoinfo[:city_name]
-puts "Latitude:\t"     + geoinfo[:latitude]
-puts "Longitude:\t"    + geoinfo[:longitude]
-puts "Time zone:\t"    + geoinfo[:timezone]
+geoip = GeoIP.new('maxmind.dat')
+geoinfo = geoip.country(ip).to_h
+
+puts "IP address:\t"   + geoinfo[:ip].to_s
+puts "Country:\t"      + geoinfo[:country_name].to_s
+puts "Country code:\t" + geoinfo[:country_code2].to_s
+puts "City name:\t"    + geoinfo[:city_name].to_s
+puts "Latitude:\t"     + geoinfo[:latitude].to_s
+puts "Longitude:\t"    + geoinfo[:longitude].to_s
+puts "Time zone:\t"    + geoinfo[:timezone].to_s
