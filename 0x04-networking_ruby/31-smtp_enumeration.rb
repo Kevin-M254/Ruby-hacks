34d6afcfc8c4c8940a6c0a9bd5ab6d893c1f8a00
Kevin-M254 <kmburu007@gmail.com>
2025-03-19 16:24:37 +0300

    SMTP enumeration

---
+++ 0x04-networking_ruby/31-smtp_enumeration.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'socket'
+
+users =
+  %w{
+      root rubyfu www apache2 bin daemon sshd
+      gdm nobody ftp operator postgres mysqld
+    }
+found = []
+
+@s = TCPSocket.new('192.168.0.19', 25)
+@banner = @s.recv(1024).chomp
+users.each do |user|
+  @s.send "VRFY #{user} \n\r", 0
+  resp = @s.recv(1024).chomp
+  found << user if resp.split[2] == user
+end
+@s.close
+
+puts "[+] Result:-"
+puts "[+] Banner:-"
+puts "[+] Found users: \n#{found.join("\n")}"
