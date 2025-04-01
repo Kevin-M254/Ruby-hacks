ac209ce66ca608505e44c98978043a98a17bf9a9
Kevin-M254 <kmburu007@gmail.com>
2025-04-01 19:31:26 +0300

    telegram API example

---
+++ 0x07-ruby_web/26-telegram_api.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'telegram/bot'
+
+token = ARGV[0]
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello! What can I do you for #{message.from.first_name}?")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
